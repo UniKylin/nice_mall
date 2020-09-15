c394399071a851fb1b484a9b9d9b3db37fb49696
Kylin <kylin@KylindeMacBook-Pro.local>
2020-09-15 14:36:28 +0800

    refactor: bottom nav tabs

--- lib/pages/tabs/tabs.dart
+++ lib/pages/tabs/tabs.dart
@@ -36,11 +36,22 @@ class _TabsPageState extends State<TabsPage> {
           },
           type: BottomNavigationBarType.fixed,
           items: [
-            BottomNavigationBarItem(title: Text('首页'), icon: Icon(Icons.home)),
-            BottomNavigationBarItem(title: Text('分类'), icon: Icon(Icons.category)),
             BottomNavigationBarItem(
-                title: Text('购物车'), icon: Icon(Icons.shopping_cart)),
-            BottomNavigationBarItem(title: Text('我的'), icon: Icon(Icons.person)),
+              title: Text('首页'),
+              icon: Icon(Icons.home)
+            ),
+            BottomNavigationBarItem(
+              title: Text('分类'),
+              icon: Icon(Icons.category)
+            ),
+            BottomNavigationBarItem(
+              title: Text('购物车'),
+              icon: Icon(Icons.shopping_cart)
+            ),
+            BottomNavigationBarItem(
+              title: Text('我的'),
+              icon: Icon(Icons.person)
+            ),
           ]
         ),
       );
