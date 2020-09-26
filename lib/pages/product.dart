5ccdb6db05ec8f25e4c3d51eec47e717ed9d0a85
Kylin <kylin@KylindeMacBook-Pro.local>
2020-09-26 23:13:41 +0800

    product list page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 
 class ProductPage extends StatefulWidget {
   Map arguments;
@@ -11,12 +12,97 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
     return Scaffold(
       appBar: AppBar(
         title: Text('商品列表'),
         backgroundColor: Colors.red,
       ),
-      body: Text('${widget.arguments}'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(
+            ScreenUtil().setWidth(20),
+            ScreenUtil().setHeight(15),
+            ScreenUtil().setWidth(20),
+            ScreenUtil().setHeight(15)),
+        child: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: ScreenUtil().setWidth(180),
+                        height: ScreenUtil().setHeight(180),
+                        child: Image.network(
+                          'https://www.itying.com/images/flutter/list2.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          height: ScreenUtil().setHeight(180),
+                          margin:
+                              EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)',
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: ScreenUtil().setSp(28),
+                                ),
+                              ),
+                              Text(
+                                '￥880',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: ScreenUtil().setSp(26),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                        right: ScreenUtil().setWidth(20)),
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: ScreenUtil().setWidth(20),
+                                      vertical: ScreenUtil().setHeight(5),
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        width: 1,
+                                        color: Colors.red,
+                                      ),
+                                      borderRadius: BorderRadius.circular(
+                                          ScreenUtil().setWidth(20)),
+                                    ),
+                                    child: Text(
+                                      '官方自营',
+                                      style: TextStyle(
+                                        fontSize: ScreenUtil().setSp(20),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 20,
+                  )
+                ],
+              );
+            }),
+      ),
     );
   }
 }
