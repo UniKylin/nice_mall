fa4ce73dde543acd4367f498d9aa07a7367400ee
Kylin <kylin@KylindeMacBook-Pro.local>
2020-10-08 10:10:02 +0800

    common header layout

--- lib/pages/tabs/tabs.dart
+++ lib/pages/tabs/tabs.dart
@@ -3,6 +3,7 @@ import 'package:nicemart/pages/tabs/cart.dart';
 import 'package:nicemart/pages/tabs/category.dart';
 import 'package:nicemart/pages/tabs/home.dart';
 import 'package:nicemart/pages/tabs/mine.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 
 class TabsPage extends StatefulWidget {
   TabsPage({Key key}) : super(key: key);
@@ -21,7 +22,6 @@ class _TabsPageState extends State<TabsPage> {
   @override
   void initState() {
     super.initState();
-    print('>>>>>>>>>>>>>>>>>>> tab page init');
     _pageController = new PageController(
       keepPage: true,
       initialPage: this._currentIndex,
@@ -36,30 +36,81 @@ class _TabsPageState extends State<TabsPage> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('麒麟商场'),
-          backgroundColor: Colors.red,
-        ),
-        body: PageView(
-          controller: _pageController,
-          children: <Widget>[
-            HomePage(),
-            CategoryPage(),
-            CartPage(),
-            MinePage(),
-          ],
-          onPageChanged: (index) {
-            print('>>>>>>>>>>> page changed...${index}');
-            setState(() {
-              this._currentIndex = index;
-            });
-          },
+      appBar: (
+        (this._currentIndex == 0 || this._currentIndex == 1) ? (
+          AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.center_focus_weak,
+                size: 28,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/search');
+              },
+            ),
+            title: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/search');
+              },
+              child: Container(
+                height: ScreenUtil().setHeight(52),
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(
+                  horizontal: ScreenUtil().setWidth(20),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: ScreenUtil().setWidth(52),
+                      color: Colors.red,
+                    ),
+                    Text(
+                      '小米笔记本',
+                      style: TextStyle(
+                        fontSize: ScreenUtil().setSp(28),
+                        color: Colors.black54,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            backgroundColor: Colors.red,
+            actions: [],
+          )
+        ) : (
+          AppBar(
+            title: Text('这就是一个表头啊'),
+          )
+        )
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          HomePage(),
+          CategoryPage(),
+          CartPage(),
+          MinePage(),
+        ],
+        onPageChanged: (index) {
+          print('>>>>>>>>>>> page changed...${index}');
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
 
-          // 禁止左右滑动，体验下降
-          // physics: NeverScrollableScrollPhysics(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
+        // 禁止左右滑动，体验下降
+        // physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
           currentIndex: this._currentIndex,
           onTap: (index) {
             print('>>>>>>>>>>> bottom changed...${index}');
@@ -76,39 +127,29 @@ class _TabsPageState extends State<TabsPage> {
           fixedColor: Colors.red,
           type: BottomNavigationBarType.fixed,
           items: [
+            BottomNavigationBarItem(title: Text('首页'), icon: Icon(Icons.home)),
             BottomNavigationBarItem(
-              title: Text('首页'),
-              icon: Icon(Icons.home)
-            ),
+                title: Text('分类'), icon: Icon(Icons.category)),
             BottomNavigationBarItem(
-              title: Text('分类'),
-              icon: Icon(Icons.category)
-            ),
+                title: Text('购物车'), icon: Icon(Icons.shopping_cart)),
             BottomNavigationBarItem(
-              title: Text('购物车'),
-              icon: Icon(Icons.shopping_cart)
-            ),
-            BottomNavigationBarItem(
-              title: Text('我的'),
-              icon: Icon(Icons.person)
-            ),
-          ]
-        ),
-        // floatingActionButton: Container(
-        //   height: 80,
-        //   width: 80,
-        //   margin: EdgeInsets.all(10),
-        //   padding: EdgeInsets.all(10),
-        //   decoration: BoxDecoration(
-        //     borderRadius: BorderRadius.circular(40)
-        //   ),
-        //   child: FloatingActionButton(
-        //     child: Icon(Icons.add),
-        //     backgroundColor: Colors.pink,
-        //     mini: true,
-        //   ),
-        // ),
-        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      );
+                title: Text('我的'), icon: Icon(Icons.person)),
+          ]),
+      // floatingActionButton: Container(
+      //   height: 80,
+      //   width: 80,
+      //   margin: EdgeInsets.all(10),
+      //   padding: EdgeInsets.all(10),
+      //   decoration: BoxDecoration(
+      //     borderRadius: BorderRadius.circular(40)
+      //   ),
+      //   child: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //     backgroundColor: Colors.pink,
+      //     mini: true,
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
