57563054cb115188669c44d3fe92f2e5f0a59426
Kylin <kylin@KylindeMacBook-Pro.local>
2020-10-08 11:46:42 +0800

    cart & mine page safe top header height

--- lib/pages/tabs/cart.dart
+++ lib/pages/tabs/cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartPage extends StatefulWidget {
   CartPage({Key key}) : super(key: key);
@@ -20,6 +21,12 @@ class _CartPageState extends State<CartPage> with AutomaticKeepAliveClientMixin 
 
   @override
   Widget build(BuildContext context) {
-    return Text('购物车...');
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
+    double safeTopHeight = MediaQuery.of(context).padding.top;
+
+    return Container(
+      margin: EdgeInsets.only(top: safeTopHeight,),
+      child: Text('购物车'),
+    );
   }
 }
--- lib/pages/tabs/mine.dart
+++ lib/pages/tabs/mine.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MinePage extends StatefulWidget {
   MinePage({Key key}) : super(key: key);
@@ -19,6 +20,12 @@ class _MinePageState extends State<MinePage> with AutomaticKeepAliveClientMixin{
   }
   @override
   Widget build(BuildContext context) {
-    return Text('个人中心...');
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
+    double safeTopHeight = MediaQuery.of(context).padding.top;
+
+    return Container(
+      margin: EdgeInsets.only(top: safeTopHeight,),
+      child: Text('个人中心'),
+    );
   }
 }
